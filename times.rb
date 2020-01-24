cf6634b4194dab3c1533eaead36d4731250ef11a
Xavier Perez <xperez007@gmail.com>
2020-01-24 13:01:21 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,30 @@
+5.times do
+  puts "Penguins like to jump 
+off icebergs !"
+end
 def using_times
 	#your code here
 end
 
+7.times do
+  puts " I am doing the dishes
+left by my former friends."
+end
+
+jewels_in_the_bag = 100
+
+3.times do
+  puts "Hiding 10 stolen
+jewels."
+  jewels_in_the_bag =
+jewels_in_the_bag
+end
+
+#=> 3 (return value)
+
+7.times do
+  puts "Wingdarium Leviosa"
+end
 
 
 
